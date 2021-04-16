42ffd0191e54809b3f68fd1d37e5c47b866e091a
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-04-16 15:18:51 -0300

    [SP17] - Exercício 9 - Spring boot

---
+++ programando-spring-boot/src/main/java/com/devinhouse/programandospringboot/ProgramandoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.programandospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramandoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramandoSpringBootApplication.class, args);
+	}
+
+}
---
+++ programando-spring-boot/src/test/java/com/devinhouse/programandospringboot/ProgramandoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.programandospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramandoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
